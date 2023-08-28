8a7a4df7c31a3ebf65d3a63b73b7a353bfbd7103
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-08-28 14:24:24 -0700

    updated app.js, fixed bug in app.test.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <div id='event-list'></div>
+    </div>
+  );
+};
 
 export default App;
--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -4,6 +4,6 @@ import App from '../App';
 describe('<App /> component', () => {
   test('renders list of events', () => {
     const AppDOM = render(<App />).container;
-    expect(AppDOM.queryselector('#event-list')).toBeInTheDocument();
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
   });
 });
