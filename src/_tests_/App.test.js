232f02986bdb89cd514b9e859f7932bca70a0f0e
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-08-29 10:24:29 -0700

    Created and tested CitySearch

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -5,6 +5,11 @@ import App from '../App';
 describe('<App /> component', () => {
   test('renders list of events', () => {
     const AppDOM = render(<App />).container;
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument(); // .toBeInTheDocument is called a MATCHER function
+  });
+
+  test('render CitySearch', () => {
+    const AppDOM = render(<App />).container;
+    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
   });
 });
