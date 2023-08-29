00030585f1819298713db11ed93ee170d7d1449c
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-08-29 14:50:36 -0700

    updated setupTest.js and CitySearch.js 7/7 test pass

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 const CitySearch = () => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
   return (
     <div id='city-search'>
-      <input type='text' className='city' placeholder='Search for city' />
+      <input
+        type='text'
+        className='city'
+        placeholder='Search for city'
+        onFocus={() => setShowSuggestions(true)}
+      />
+      {showSuggestions ? <ul className='suggestions'></ul> : null}
     </div>
   );
 };
