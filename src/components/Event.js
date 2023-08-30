9c7181de275fc2d464507b621b571a7946784d9d
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-08-30 15:53:01 -0700

    updating tests folder 4.4

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,12 @@
-const Event = () => {
-  return <li></li>;
+const Event = ({ event }) => {
+  return (
+    <li>
+      <h3>{event.summary}</h3>
+      <p>{event.created}</p>
+      <p>{event.location}</p>
+      <button>Show Details</button>
+    </li>
+  );
 };
 
 export default Event;
