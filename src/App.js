4f1f92f0a1273107a0075404dbba8ef2f1327af6
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-08-31 12:49:44 -0700

    updated app.js and eventlist.test.js 18/18

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState, useEffect } from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { getEvents } from './api';
 import './App.css';
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  // const [allLocations, setAllLocations] = useState([]);
+  // const [currentCity, setCurrentCity] = useState();
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE)); //this takes the first 32 events
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
       <NumberOfEvents />
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 };
